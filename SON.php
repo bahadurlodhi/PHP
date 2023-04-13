6c89295ac1ec4aa1688001ab0f0e115e04418613
bahadur  lodhi <112106562+bahadurlodhi@users.noreply.github.com>
2023-04-13 03:50:28 -0400

    Add files via upload

---
+++ SON.php
@@ -0,0 +1,9 @@
+<?php
+$a=$_POST['num1'];
+$b=$_POST['num2'];
+$gender=$_POST['gender'];
+$password=$_POST["password"];
+echo "The sum is:",$a+$b;
+echo "<br>Gender:",$gender;
+echo "<br>your password is: $password";  
+?>
