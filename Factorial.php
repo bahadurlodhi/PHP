4cba1366d5eab2036f990482dec9dce61731cdc6
bahadur  lodhi <112106562+bahadurlodhi@users.noreply.github.com>
2023-05-01 12:30:22 -0400

    Add files via upload

---
+++ Factorial.php
@@ -0,0 +1,80 @@
+<?php  
+$n = 1;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The factorial of $n! = $f","<br>";  
+$n = 2;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","</br>"; 
+
+$n = 3;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 4;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 5;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 6;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 7;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 8;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 9;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+
+$n = 10;  
+$f = 1;  
+for ($i=$n; $i>=1; $i--)   
+{  
+$f = $f * $i;  
+}  
+echo "The Factorial of $n! = $f","<br>";  
+?>
